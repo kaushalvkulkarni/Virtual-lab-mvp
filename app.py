dee2998c6bf631f95fbef3959dd42ec0f4eae7d2
kaushalvkulkarni <kaushalvk0@gmail.com>
2026-08-08 16:15:45 +0530

    Add RandomForestClassifier and data processing imports

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st 
-import pandas as pd 
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.modelselection import train_test_split
+from sklearn.metrics import accuracy_score 
 st.title("Virtual Bioinformatics Lab")
 st.write("Welcome to the first live test of my AI/ML startup")
 st.header("Module 1: Data Ingestion")
@@ -9,3 +12,4 @@ if uploaded_file is not None:
    df = pd.read_csv(uploaded_file)
    st.success("Data succesfully loaded!")
    st.dataframe(df)
+   
