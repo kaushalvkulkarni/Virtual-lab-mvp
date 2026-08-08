2e8be2a3d6a4ea1a41169cd7279012de95686d1f
kaushalvkulkarni <kaushalvk0@gmail.com>
2026-08-08 12:47:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st 
+import pnadas as pd 
 st.title("Virtual Bioinformatics Lab")
 st.write("Welcome to the first live test of my AI/ML startup")
+st.header("Module 1: Data Ingestion")
+st.write("upload a biologival dataset (CSV format) to begine analysis")
+uploaded_file = st.file_uploder("Drop your CSV file here", type=["CSV"])
+if uploded file is not none:
+   df = pd.read_CSV(uplode_file)
+   st.success("Data succesfully loaded!")
+   st.dataframe("df")
