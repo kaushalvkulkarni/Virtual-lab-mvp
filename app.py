b67cb675b3acbaaa7272f6e5b7ac845378cc1875
kaushalvkulkarni <kaushalvk0@gmail.com>
2026-08-08 12:50:55 +0530

    Correct typos in data ingestion logic
    Fix typos in file upload handling and messages.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.write("Welcome to the first live test of my AI/ML startup")
 st.header("Module 1: Data Ingestion")
 st.write("upload a biologival dataset (CSV format) to begine analysis")
 uploaded_file = st.file_uploder("Drop your CSV file here", type=["CSV"])
-if uploded file is not none:
+if uploded_file is not None:
    df = pd.read_CSV(uplode_file)
    st.success("Data succesfully loaded!")
    st.dataframe("df")
