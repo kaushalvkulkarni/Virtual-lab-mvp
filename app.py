27b0b31af42326fc6971ca2466ecc3ef9fb004d6
kaushalvkulkarni <kaushalvk0@gmail.com>
2026-08-08 12:53:07 +0530

    Correct file uploader function and variable names
    Fix typo in file uploader function and variable names.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Virtual Bioinformatics Lab")
 st.write("Welcome to the first live test of my AI/ML startup")
 st.header("Module 1: Data Ingestion")
 st.write("upload a biologival dataset (CSV format) to begine analysis")
-uploaded_file = st.file_uploder("Drop your CSV file here", type=["CSV"])
+uploaded_file = st.file_uploader("Drop your CSV file here", type=["CSV"])
 if uploded_file is not None:
    df = pd.read_CSV(uplode_file)
    st.success("Data succesfully loaded!")
