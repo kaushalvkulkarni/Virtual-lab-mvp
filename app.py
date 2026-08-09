cb82c51a6940f2953019d25cc779eba1bb70b31e
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-09 12:47:56 +0530

    Fix import statement for LogisticRegression

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn_model import LogisticRegression 
+from sklearn.linear_model import LogisticRegression 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score 
 st.title("Virtual Bioinformatics Lab")
